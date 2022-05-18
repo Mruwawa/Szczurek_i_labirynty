d31ee3b9324994157699f2c123600d8dcf71b290
StaraMyrta <dziobakmarrta@gmail.com>
2022-05-18 21:44:57 +0200

    blabla
    lwowowow

---
+++ src/main/java/mian.java
@@ -0,0 +1,9 @@
+public class mian {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        powiedz_cos();
+    }
+    static void powiedz_cos() {
+        System.out.println("RYCZĘĘĘĘ");
+    }
+}
