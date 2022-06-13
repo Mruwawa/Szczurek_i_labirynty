7ae1557dbc2d118a49e22e06e7dde574cb484020
StaraMyrta <dziobakmarrta@gmail.com>
2022-06-14 00:02:42 +0200

    commitowanie przeprowadzone

--- src/main/java/Szczurki/Simulation/EntityPositioner.java
+++ src/main/java/Szczurki/Simulation/EntityPositioner.java
@@ -2,6 +2,7 @@ package Szczurki.Simulation;
 
 import Szczurki.Configuration.SimulationSettings;
 import Szczurki.Simulation.Entities.Animals.*;
+import Szczurki.Simulation.Entities.Guardian;
 import Szczurki.Simulation.Entities.Interfaces.IEntity;
 import Szczurki.Simulation.Entities.Interfaces.IUpdatable;
 import Szczurki.Utilities.Vector;
@@ -21,51 +22,56 @@ public class EntityPositioner implements IEntityPositioner {
 
     @Override
     public void placeEntities(IEntity[][] map, List<IUpdatable> updatableEntities) {
+//umieszczenie strażnika na mapie
+
 
         _settings.animalCounts.forEach((animalName, count) ->
         {
+            //rozmieszczenie zwierząt na mapie
             for (int i = 0; i < count; i++) {
+
                 Vector place;
                 do {
                     place = Vector.getRandomVector(_settings.mapWidth, _settings.mapHeight);
                 } while (map[place.x][place.y] != null);
 
-                Animal animal = null;
-
-                switch (animalName) {
-                    case "gerbils":
-                        animal = new Gerbil(place.x, place.y, pickName());
-                        break;
-                    case "hamsters":
-                        animal = new Hamster(place.x, place.y, pickName());
-                        break;
-                    case "mice":
-                        animal = new Mouse(place.x, place.y, pickName());
-                        break;
-                    case "mousedeer":
-                        animal = new Mousedeer(place.x, place.y, pickName());
-                        break;
-                    case "rats":
-                        animal = new Rat(place.x, place.y, pickName());
-                        break;
-                }
+                Animal animal = switch (animalName) {
+                    case "gerbils" -> new Gerbil(place.x, place.y, pickName());
+                    case "hamsters" -> new Hamster(place.x, place.y, pickName());
+                    case "mice" -> new Mouse(place.x, place.y, pickName());
+                    case "mousedeer" -> new Mousedeer(place.x, place.y, pickName());
+                    case "rats" -> new Rat(place.x, place.y, pickName());
+                    default -> null;
+                };
 
                 map[place.x][place.y] = animal;
                 updatableEntities.add(animal);
             }
         });
+
+        Vector guardianPlace;
+
+        do {
+            guardianPlace = Vector.getRandomVector(_settings.mapWidth, _settings.mapHeight);
+        } while (map[guardianPlace.x][guardianPlace.y] != null);
+
+        Guardian guardian = new Guardian(guardianPlace.x, guardianPlace.y);
+
+        map[guardianPlace.x][guardianPlace.y] = guardian;
+        updatableEntities.add(guardian);
     }
 
-    private String pickName() {
-        if (_possibleNames.size() == 0) _possibleNames = getPossibleNames();
+        private String pickName () {
+            if (_possibleNames.size() == 0) _possibleNames = getPossibleNames();
 
-        var random = new Random();
-        var index = random.nextInt(_possibleNames.size());
-        var pickedName = _possibleNames.get(index);
-        _possibleNames.remove(pickedName);
+            var random = new Random();
+            var index = random.nextInt(_possibleNames.size());
+            var pickedName = _possibleNames.get(index);
+            _possibleNames.remove(pickedName);
 
-        return pickedName;
-    }
+            return pickedName;
+        }
+
 
 
     private ArrayList<String> getPossibleNames() {
